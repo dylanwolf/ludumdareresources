6e47e01b8d08cfb19369a96889d3d4a89eca9a6b
dylan <dylan@dylanwolf.com>
2016-05-01 21:52:43 -0400

    Added basic platformer controller

---
+++ Platforming/PlatformerPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformerPlayer : PlatformerCharacter {
+
+	const string INPUT_HORIZONTAL = "Horizontal";
+	const string INPUT_JUMP = "Jump";
+	InputResponse tmpInput = new InputResponse();
+
+	protected override InputResponse? GetInput()
+	{
+		tmpInput.HorizontalDirection = Input.GetAxisRaw(INPUT_HORIZONTAL);
+		tmpInput.Jump = Input.GetButton(INPUT_JUMP);
+
+		return tmpInput;
+    }
+
+}
